7828a64e40be9e4dc6e31bf75b21728f33f4cf22
TreeBomb-01 <htb010630@gmail.com>
2025-04-15 19:57:49 +0900

    mapper 생성
    수정 매우 필요

---
+++ src/main/java/com/levelupfit/mainbackend/mapper/FormUserMapper.java
@@ -0,0 +1,16 @@
+package com.levelupfit.mainbackend.mapper;
+
+import com.levelupfit.mainbackend.dto.FormUserDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FormUserMapper {
+
+    //formUser 저장
+    void save(FormUserDTO formuserdto);
+
+    //FormUser찾기
+    FormUserDTO findById(String userId);
+
+    void findPassword(String userId, String passwd);
+}
--- src/main/java/com/levelupfit/mainbackend/mapper/UserMapper.java
+++ src/main/java/com/levelupfit/mainbackend/mapper/UserMapper.java
@@ -1,5 +1,26 @@
 package com.levelupfit.mainbackend.mapper;
 
-public class UserMapper {
+import com.levelupfit.mainbackend.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    
+    //유저 저장
+    void save (UserDTO userDto);
+
+    //유저 찾기 (user_id)
+    UserDTO findByUserId(int user_id);
+
+    //리프레쉬 토큰 업데이트
+    void updateAccessTokenAndRefreshToken(int userId, String accessToken, String refreshToken);
+
+    void updateAccessToken(String userId, String accessToken);
+
+    void updateRefreshToken(String userId, String refreshToken);
+
+    void invalidateAccessToken(String accessToken);
+
+    UserDTO findByRefreshToken(String refreshToken);
 
 }

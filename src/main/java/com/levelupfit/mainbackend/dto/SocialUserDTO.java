10bfeacaed28d5dd016c9b56de5f77a2bf834542
TreeBomb-01 <htb010630@gmail.com>
2025-04-15 19:57:06 +0900

    DTO 생성
     수정 필요

---
+++ src/main/java/com/levelupfit/mainbackend/dto/SocialUserDTO.java
@@ -0,0 +1,19 @@
+package com.levelupfit.mainbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class SocialUserDTO {
+
+    private String userId;
+    private String providerType;
+    private String email;
+}
--- src/main/java/com/levelupfit/mainbackend/dto/UserDTO.java
+++ src/main/java/com/levelupfit/mainbackend/dto/UserDTO.java
@@ -4,11 +4,13 @@ import lombok.Data;
 
 @Data
 public class UserDTO {
-    private int id;
+    private int user_id;
     private String email;
     private String nickname;
     private String dob;
-    private String level;
+    private int level;
     private String gender;
     private String profile;
+    private String accessToken;
+    private String refreshToken;
 }

3fff5048503044e730914a1d69b6880ebd41c1c5
TreeBomb-01 <htb010630@gmail.com>
2025-04-15 19:58:35 +0900

    보안 설정 파일 생성
    공부 필요

--- src/main/java/com/levelupfit/mainbackend/config/SecurityConfig.java
+++ src/main/java/com/levelupfit/mainbackend/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.levelupfit.mainbackend.config;
 
+import com.levelupfit.mainbackend.mapper.FormUserMapper;
+import com.levelupfit.mainbackend.mapper.UserMapper;
+import com.levelupfit.mainbackend.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,6 +25,8 @@ public class SecurityConfig {
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AuthenticationConfiguration authenticationConfiguration;
+    private final UserMapper userMapper;
+    private final FormUserMapper formUserMapper;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,17 +37,26 @@ public class SecurityConfig {
         return http.build();
     }
 
+    //인증을 처리하려면 AuthenticationManager 가 필요함
+    //인증이 진행되는 곳
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return authenticationConfiguration .getAuthenticationManager();
     }
 
+
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        //authProvider.setUserDetailsService(userDetailsService()); //Srvice 코드 작성 해야함
-        authProvider.setPasswordEncoder(bCryptPasswordEncoder);   //Srvice 코드 작성 해야함
+        authProvider.setUserDetailsService(userDetailsService()); //
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder); //비밀번호 일치 여부 판단
         return authProvider;
+    }
+
+    // Spring Security 가 사용할 사용자 정보를 CustomUserDetails 로 반환하도록 설정 (기본 설정 UserDetails)
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(userMapper, formUserMapper);
     }
 
 }

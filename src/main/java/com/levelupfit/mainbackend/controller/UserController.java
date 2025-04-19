4aa4adac02a7780d3a7751fcfc70833fbf8b8657
TreeBomb-01 <htb010630@gmail.com>
2025-04-19 23:37:36 +0900

    카카오 로그인 로직 구현

--- src/main/java/com/levelupfit/mainbackend/controller/UserController.java
+++ src/main/java/com/levelupfit/mainbackend/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.levelupfit.mainbackend.controller;
 
-import com.levelupfit.mainbackend.dto.FormInfoDTO;
-import com.levelupfit.mainbackend.dto.FormUserDTO;
-import com.levelupfit.mainbackend.dto.LoginRequestDTO;
-import com.levelupfit.mainbackend.dto.UserDTO;
+import com.levelupfit.mainbackend.dto.*;
+import com.levelupfit.mainbackend.service.KakaoService;
 import com.levelupfit.mainbackend.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,20 +9,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
+    private final KakaoService kakaoService;
 
     @PostMapping("/register")
     public ResponseEntity<Map<String, String>> saveFormUser(@RequestBody FormInfoDTO formInfoDto, HttpServletResponse response) {
@@ -42,38 +39,78 @@ public class UserController {
         int signed = userService.saveFormUser(formUserDto, userDto, response);
 
         Map<String, String> responseMap = new HashMap<>();
-        switch (signed){
-            case 201:
-                //responseMap.put("status", "201");
+        return switch (signed) {
+            case 201 -> {
                 responseMap.put("message", "회원가입이 완료되었습니다.");
-                return ResponseEntity
-                        .status(HttpStatus.CREATED)
-                        .body(responseMap);
-            case 409:
-                //responseMap.put("status", "409");
+                yield ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+            }
+            case 409 -> {
                 responseMap.put("message", "중복된 이메일 입니다.");
-                return ResponseEntity
-                        .status(HttpStatus.CONFLICT)
-                        .body(responseMap);
-            case 500:
-                responseMap.put("status", "500");
+                yield ResponseEntity.status(HttpStatus.CONFLICT).body(responseMap);
+            }
+            case 410 -> {
+                responseMap.put("message", "이미 카카오로그인으로 등록된 이메일입니다.");
+                yield ResponseEntity.status(HttpStatus.CONFLICT).body(responseMap);
+            }
+            case 500 -> {
                 responseMap.put("message", "회원가입중 오류가 발생했습니다.");
-        }
-//        if (signed) {
-//            responseMap.put("message", "회원가입이 완료되었습니다. 기존 소셜 계정과 자동 연동되었습니다.");
-//        } else {
-//            responseMap.put("message", "회원가입이 성공적으로 완료되었습니다.");
-//        }
-//        responseMap.put("userId", Integer.toString(userDto.getUser_id()));
+                yield ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMap);
+            }
+            default -> {
+                responseMap.put("message", "정의되지 않은 오류가 발생했습니다.");
+                yield ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
+            }
+        };
 
-        return ResponseEntity.ok(responseMap);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDTO dto){
-        if("test".equals(dto.getEmail()) && "test".equals(dto.getPassword())){
-            return ResponseEntity.ok("Success");
-        }
-        else return ResponseEntity.ok("Fail");
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDTO dto, HttpServletResponse response){
+
+        int login = userService.login(dto);
+
+        Map<String, String> responseMap = new HashMap<>();
+        return switch (login) {
+            case 200 -> {
+                responseMap.put("message", "로그인 성공");
+                yield ResponseEntity.status(HttpStatus.OK).body(responseMap);
+            }
+            case 401 -> {
+                responseMap.put("message", "아이디 혹은 비밀번호가 일치하지 않습니다.");
+                yield ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseMap);
+            }
+            default -> {
+                responseMap.put("message", "정의되지 않은 오류가 발생했습니다.");
+                yield ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
+            }
+        };
+    }
+
+    //카카오 로그인 페이지 처리
+    @GetMapping("/kakao/login")
+    public ResponseEntity<String> loginPage(){
+        String kakaoLoginUrl = kakaoService.loginPage();
+        return ResponseEntity.ok(kakaoLoginUrl);
+    }
+
+    //카카오 로그인 처리가 이루어지는 곳
+    @GetMapping("/callback")
+    public ResponseEntity<Map<String, String>> checkUser(@RequestParam("code") String code) {
+        int result = kakaoService.handleKakaoLogin(code);
+        Map<String, String> responseMap = new HashMap<>();
+        return switch (result) {
+            case 200 -> {
+                responseMap.put("message", "로그인 성공");
+                yield ResponseEntity.status(HttpStatus.OK).body(responseMap);
+            }
+            case 401 -> {
+                responseMap.put("message", "아이디 혹은 비밀번호가 일치하지 않습니다.");
+                yield ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseMap);
+            }
+            default -> {
+                responseMap.put("message", "정의되지 않은 오류가 발생했습니다.");
+                yield ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
+            }
+        };
     }
 }

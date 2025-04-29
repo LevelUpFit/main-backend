2f42380ff7d77046f0d8396bc2e439fa6452f6c2
TreeBomb-01 <htb010630@gmail.com>
2025-04-29 18:40:39 +0900

    create domain

--- src/main/java/com/levelupfit/mainbackend/controller/RoutineController.java
+++ src/main/java/com/levelupfit/mainbackend/controller/RoutineController.java
@@ -1,5 +1,14 @@
 package com.levelupfit.mainbackend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin(origins = "http://localhost:4000")
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("routine")
 public class RoutineController {
 
 }

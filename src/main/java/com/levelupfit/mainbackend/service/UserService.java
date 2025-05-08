0ea9f4cb7e69ba588902d816ccc26fbe05cb0af1
TreeBomb-01 <htb010630@gmail.com>
2025-05-08 14:50:43 +0900

    안쓰는 import문 정리

--- src/main/java/com/levelupfit/mainbackend/service/UserService.java
+++ src/main/java/com/levelupfit/mainbackend/service/UserService.java
@@ -14,7 +14,6 @@ import com.levelupfit.mainbackend.repository.SocialUserRepository;
 import com.levelupfit.mainbackend.repository.UserRepository;
 import com.levelupfit.mainbackend.repository.UserStrengthRepository;
 import com.levelupfit.mainbackend.util.JwtUtils;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -24,8 +23,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor

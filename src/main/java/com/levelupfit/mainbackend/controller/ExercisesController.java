2c5981e57c1425b1cdb3b5e5fa34c2f2a710fce6
TreeBomb-01 <htb010630@gmail.com>
2025-05-19 17:43:21 +0900

    Exercise 로직 구현중

--- src/main/java/com/levelupfit/mainbackend/controller/ExercisesController.java
+++ src/main/java/com/levelupfit/mainbackend/controller/ExercisesController.java
@@ -1,5 +1,24 @@
 package com.levelupfit.mainbackend.controller;
 
+import com.levelupfit.mainbackend.domain.exercise.Exercise;
+import com.levelupfit.mainbackend.dto.ApiResponse;
+import com.levelupfit.mainbackend.dto.exercise.ExerciseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4000")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/exercise")
 public class ExercisesController {
 
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<ExerciseDTO>> createExercise(Exercise exercise) {
+        ExerciseDTO exerciseDTO = new ExerciseDTO(); //ApiResponse DTO로 반환 하는 로직 구현하기
+        return null;
+    }
 }
---
+++ src/main/java/com/levelupfit/mainbackend/dto/exercise/ExerciseDTO.java
@@ -0,0 +1,10 @@
+package com.levelupfit.mainbackend.dto.exercise;
+
+public class ExerciseDTO {
+    private int id;
+    private String name;
+    private String description;
+    private String thumbnailUrl;
+    private String targetMuscle;
+    private boolean feedbackAvailable;
+}

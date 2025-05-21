b19ea7b26561854746dea76c229a2c024147f1fa
TreeBomb-01 <htb010630@gmail.com>
2025-05-21 20:01:21 +0900

    공통 반환 DTO 생성

--- src/main/java/com/levelupfit/mainbackend/dto/ApiResponse.java
+++ src/main/java/com/levelupfit/mainbackend/dto/ApiResponse.java
@@ -9,17 +9,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ApiResponse<T> {
     private boolean success;
+    private int code;
     private String message;
     private T data;
 
     // 성공 응답용 팩토리 메서드
-    public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<>(true, "요청에 성공했습니다.", data);
+    public static <T> ApiResponse<T> ok(T data, int code) {
+        return new ApiResponse<>(true, code, "요청에 성공했습니다.", data);
     }
 
     // 실패 응답용 팩토리 메서드
-    public static <T> ApiResponse<T> fail(String message) {
-        return new ApiResponse<>(false, message, null);
+    public static <T> ApiResponse<T> fail(String message, int code) {
+        return new ApiResponse<>(false, code, message, null);
     }
     
     //이걸로 반환하는거 먼저 해보고 고치기

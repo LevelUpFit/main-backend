0ec8be9d9fb581c8fe9bdfad7ce94db23700695b
TreeBomb-01 <htb010630@gmail.com>
2025-05-22 07:51:46 +0900

    엔티티 DTO 변환 함수 생성
    Exercise 엔티티의 데이터를 전송하기 위한 DTO 생성.
    
    백엔드와 프론트엔드 간 운동 정보 전달을 용이하게 하며,
    id, 이름, 설명, 썸네일 URL, 타겟 근육, 피드백 가능 여부 등의 필드 포함.
    
    Exercise 도메인 객체로부터 DTO를 생성하는 정적 팩토리 메서드 추가.

--- src/main/java/com/levelupfit/mainbackend/dto/exercise/ExerciseDTO.java
+++ src/main/java/com/levelupfit/mainbackend/dto/exercise/ExerciseDTO.java
@@ -1,5 +1,6 @@
 package com.levelupfit.mainbackend.dto.exercise;
 
+import com.levelupfit.mainbackend.domain.exercise.Exercise;
 import lombok.Data;
 
 @Data
@@ -10,5 +11,29 @@ public class ExerciseDTO {
     private String thumbnailUrl;
     private String targetMuscle;
     private Boolean feedbackAvailable;
+
+    public ExerciseDTO() {
+
+    }
+
+    public ExerciseDTO(int id, String name, String description, String thumbnailUrl, String targetMuscle, Boolean feedbackAvailable) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnailUrl = thumbnailUrl;
+        this.targetMuscle = targetMuscle;
+        this.feedbackAvailable = feedbackAvailable;
+    }
+
+    public static ExerciseDTO fromExercise(Exercise exercise) {
+        return new ExerciseDTO(
+                exercise.getExerciseId(),
+                exercise.getName(),
+                exercise.getDescription(),
+                exercise.getThumbnailUrl(),
+                exercise.getTargetMuscle(),
+                exercise.isFeedbackAvailable()
+        );
+    }
 
 }

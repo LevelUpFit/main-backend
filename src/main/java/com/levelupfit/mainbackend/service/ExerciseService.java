e7a190d968617028aa315301d8d572d448d7b85d
TreeBomb-01 <htb010630@gmail.com>
2025-05-21 21:07:14 +0900

    Exercise Create 로직 구현(수정 필요)
    thumbnailUrl 로직 수정 필요

--- src/main/java/com/levelupfit/mainbackend/service/ExerciseService.java
+++ src/main/java/com/levelupfit/mainbackend/service/ExerciseService.java
@@ -2,29 +2,46 @@ package com.levelupfit.mainbackend.service;
 
 import com.levelupfit.mainbackend.domain.exercise.Exercise;
 import com.levelupfit.mainbackend.dto.ApiResponse;
+import com.levelupfit.mainbackend.dto.exercise.ExerciseDTO;
 import com.levelupfit.mainbackend.dto.exercise.request.ExerciseCreateRequest;
+import com.levelupfit.mainbackend.repository.ExerciseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ExerciseService {
+    private final ExerciseRepository exerciseRepository;
 
-    public ApiResponse<ExerciseCreateRequest> ExerciseCreate(ExerciseCreateRequest exerciseDTO) {
+    public ApiResponse<ExerciseDTO> ExerciseCreate(ExerciseCreateRequest exerciseDTO) {
         //create 구현중
         try{
             Exercise exercise = Exercise.builder()
                     .name(exerciseDTO.getName())
                     .description(exerciseDTO.getDescription())
                     .targetMuscle(exerciseDTO.getTargetMuscle())
+                    .thumbnailUrl("test")
                     .feedbackAvailable(false) //기본값 false
                     .build();
 
+            Exercise createdExercise = exerciseRepository.save(exercise); //JPA 저장
+            
+            //DTO로 변환
+            ExerciseDTO createdExerciseDTO = new ExerciseDTO();
+            createdExerciseDTO.setId(createdExercise.getExerciseId());
+            createdExerciseDTO.setName(createdExercise.getName());
+            createdExerciseDTO.setDescription(createdExercise.getDescription());
+            createdExerciseDTO.setTargetMuscle(createdExercise.getTargetMuscle());
+            createdExerciseDTO.setThumbnailUrl(createdExercise.getThumbnailUrl());
+            createdExerciseDTO.setFeedbackAvailable(false);
+
+            //DTO 대입해서 API반환 DTO에 넣기
+            return ApiResponse.ok(createdExerciseDTO, 201);
+
 
         } catch (Exception e){
-            throw new RuntimeException("DB 저장중 오류", e);
-        }
+            return ApiResponse.fail("운동 생성중 오류발생",500);
 
-        return null;
+        }
     }
 }

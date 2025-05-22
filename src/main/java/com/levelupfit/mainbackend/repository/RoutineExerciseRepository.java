ecc582717105974e54680e20908401d7ca24ccc9
TreeBomb-01 <htb010630@gmail.com>
2025-05-22 09:34:07 +0900

    RoutineExercise Repository 생성

---
+++ src/main/java/com/levelupfit/mainbackend/repository/RoutineExerciseRepository.java
@@ -0,0 +1,8 @@
+package com.levelupfit.mainbackend.repository;
+
+import com.levelupfit.mainbackend.domain.routine.RoutineExercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoutineExerciseRepository extends JpaRepository<RoutineExercise, Integer> {
+
+}

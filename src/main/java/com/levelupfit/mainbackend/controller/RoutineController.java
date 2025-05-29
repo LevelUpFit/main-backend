03f3839a650087809b8ab33a072d709f3b16db64
TreeBomb-01 <htb010630@gmail.com>
2025-05-29 11:46:02 +0900

    루틴 수정 기능 활성화
    기존 루틴 수정 기능 추가
    루틴 정보를 수정하는 API 엔드포인트 도입
    수정 로직을 처리하기 위한 서비스 레이어 및 수정 요청용 DTO 추가

--- src/main/java/com/levelupfit/mainbackend/controller/RoutineController.java
+++ src/main/java/com/levelupfit/mainbackend/controller/RoutineController.java
@@ -6,6 +6,7 @@ import com.levelupfit.mainbackend.dto.ApiResponse;
 import com.levelupfit.mainbackend.dto.routine.RoutineDTO;
 import com.levelupfit.mainbackend.dto.routine.request.RoutineCreateRequest;
 import com.levelupfit.mainbackend.dto.routine.request.RoutineDeleteRequest;
+import com.levelupfit.mainbackend.dto.routine.request.RoutinePatchRequest;
 import com.levelupfit.mainbackend.service.RoutineService;
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.jdbc.Null;
@@ -56,9 +57,21 @@ public class RoutineController {
         }
     }
 
+    //루틴 삭제
     @DeleteMapping
     public ResponseEntity<ApiResponse<Void>> deleteRoutine(@RequestBody RoutineDeleteRequest routineDeleteRequest ) {
         ApiResponse<Void> response = routineService.deleteRoutine(routineDeleteRequest);
+        if(response.isSuccess()){
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    //루틴 수정
+    @PatchMapping
+    public ResponseEntity<ApiResponse<Void>> patchRoutine(@RequestBody RoutinePatchRequest routinePatchRequest) {
+        ApiResponse<Void> response = routineService.patchRoutine(routinePatchRequest);
         if(response.isSuccess()){
             return ResponseEntity.ok(response);
         } else {

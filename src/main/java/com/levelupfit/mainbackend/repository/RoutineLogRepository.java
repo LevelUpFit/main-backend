6da61b6e21a9b8548b8bf04541d0a65beb091a4b
TreeBomb-01 <htb010630@gmail.com>
2025-05-30 20:57:36 +0900

    루틴 로그 조회 및 삭제 기능 추가
    특정 사용자의 루틴 로그 조회 기능 도입

--- src/main/java/com/levelupfit/mainbackend/repository/RoutineLogRepository.java
+++ src/main/java/com/levelupfit/mainbackend/repository/RoutineLogRepository.java
@@ -3,5 +3,8 @@ package com.levelupfit.mainbackend.repository;
 import com.levelupfit.mainbackend.domain.routine.RoutineLogs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoutineLogRepository extends JpaRepository<RoutineLogs, Integer> {
+    List<RoutineLogs> findByUserId(int userId);
 }
--- src/main/java/com/levelupfit/mainbackend/service/RoutineLogService.java
+++ src/main/java/com/levelupfit/mainbackend/service/RoutineLogService.java
@@ -4,11 +4,15 @@ package com.levelupfit.mainbackend.service;
 import com.levelupfit.mainbackend.domain.routine.RoutineLogs;
 import com.levelupfit.mainbackend.dto.ApiResponse;
 import com.levelupfit.mainbackend.dto.routineLog.RoutineLogsDTO;
+import com.levelupfit.mainbackend.dto.routineLog.request.RoutineLogsDeleteRequest;
+import com.levelupfit.mainbackend.dto.routineLog.request.RoutineLogsGetRequest;
 import com.levelupfit.mainbackend.dto.routineLog.request.RoutineLogsRequest;
 import com.levelupfit.mainbackend.repository.RoutineLogRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +20,7 @@ public class RoutineLogService {
 
     final RoutineLogRepository routineLogRepository;
 
+    //루틴 기록 저장
     public ApiResponse<RoutineLogsDTO> saveRoutineLog(RoutineLogsRequest routineLogsRequest) {
         try{
             RoutineLogs routineLogs = RoutineLogs.builder()
@@ -32,6 +37,30 @@ public class RoutineLogService {
 
         } catch (Exception e){
             return ApiResponse.fail("루틴 기록중 오류 발생", 500);
+        }
+    }
+
+    //루틴 기록 불러오기
+    public ApiResponse<List<RoutineLogsDTO>> getRoutineLogs(RoutineLogsGetRequest routineLogsRequest) {
+
+        try {
+            List<RoutineLogsDTO> list = routineLogRepository.findByUserId(routineLogsRequest.getUserId())
+                    .stream()
+                    .map(RoutineLogsDTO::formRoutineLogs)
+                    .toList();
+            return ApiResponse.ok(list, 200);
+        } catch (Exception  e){
+            return ApiResponse.fail("기록 불러오기중 오류 발생", 500);
+        }
+    }
+
+    //기록 삭제
+    public ApiResponse<Void> deleteRoutineLog(RoutineLogsDeleteRequest routineLogsRequest) {
+        try{
+            routineLogRepository.deleteById(routineLogsRequest.getLogId());
+            return ApiResponse.ok(null, 200);
+        } catch (Exception e){
+            return ApiResponse.fail("삭제중 오류", 500);
         }
     }
 }

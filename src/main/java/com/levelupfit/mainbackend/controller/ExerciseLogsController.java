787ddb8ca8743f21af01ea019faec2ca33844998
TreeBomb-01 <htb010630@gmail.com>
2025-06-01 21:24:51 +0900

    운동 일지 조회 엔드포인트 추가
    사용자 ID 기반 운동 일지 조회 엔드포인트 구현
    요청 및 응답 데이터를 캡슐화하기 위한 DTO 사용
    서비스 계층에서 운동 일지를 조회하고 DTO로 매핑하여 반환

--- src/main/java/com/levelupfit/mainbackend/controller/ExerciseLogsController.java
+++ src/main/java/com/levelupfit/mainbackend/controller/ExerciseLogsController.java
@@ -2,6 +2,7 @@ package com.levelupfit.mainbackend.controller;
 
 import com.levelupfit.mainbackend.dto.ApiResponse;
 import com.levelupfit.mainbackend.dto.exerciseLog.ExerciseLogsDTO;
+import com.levelupfit.mainbackend.dto.exerciseLog.request.ExerciseLogsGetRequest;
 import com.levelupfit.mainbackend.dto.exerciseLog.request.ExerciseLogsRequest;
 import com.levelupfit.mainbackend.service.ExerciseLogsService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,9 @@ import okhttp3.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4000")
 @RestController
@@ -20,6 +24,16 @@ public class ExerciseLogsController {
     @PostMapping("/save")
     public ResponseEntity<ApiResponse<ExerciseLogsDTO>> saveExerciseLogs(@RequestBody ExerciseLogsRequest request) {
         ApiResponse<ExerciseLogsDTO> response = exerciseLogsService.saveExerciseLog(request);
+        if(response.isSuccess()){
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<ExerciseLogsDTO>>> getAllExerciseLogs(@RequestBody ExerciseLogsGetRequest request) {
+        ApiResponse<List<ExerciseLogsDTO>> response = exerciseLogsService.getExerciseLogs(request);
         if(response.isSuccess()){
             return ResponseEntity.ok(response);
         } else {
